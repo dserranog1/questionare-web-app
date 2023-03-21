eab83c784f927cf089a5395df8d60b9e068c1cd6
Daniel Serrano <dserranog@unal.edu.co>
2023-03-20 19:35:06 -0500

    feat: add design system (design is now broken)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import { UserProvider } from "./providers/UserProvider";
 import LoginRouter from "./routers/LoginRouter";
 import DashboardRouter from "./routers/DashboardRouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { fontSizes, spacing, themeColors } from "./theme/config";
 
 function App() {
+  const theme = extendTheme({
+    colors: themeColors,
+    space: spacing,
+    fontSizes: fontSizes,
+  });
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -20,11 +26,11 @@ function App() {
   return (
     <UserProvider>
       <QueryClientProvider client={queryClient}>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
           <header className="absolute top-2 right-6 font-pacifico text-5xl">
             Questionare
           </header>
-          <div className="flex min-h-screen flex-col justify-center bg-gradient-to-r from-antique-white  to-baby-blue-eyes font-roboto">
+          <div className="flex min-h-screen flex-col justify-center bg-cool-grey-200 font-roboto">
             <div className="h-full">
               <Routes>
                 <Route path="/" element={<Home />} />
